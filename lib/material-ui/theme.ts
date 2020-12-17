1c14869a9afa980cc8f18e157f0a3ab62a133f7a
camifernweh <camiladmuller@gmail.com>
2020-12-17 15:17:28 +0100

    style: add theme

--- lib/material-ui/theme.ts
+++ lib/material-ui/theme.ts
@@ -1,7 +1,40 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
-export const baseTheme = createMuiTheme({
+export let baseTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(0, 172, 172)',
+    },
+    secondary: {
+      main: 'rgb(1,131,149)',
+    },
+    text: {
+      primary: 'rgb(61, 61, 61)',
+      secondary: 'rgb(255, 255, 255)',
+    },
+  },
   typography: {
+    fontFamily: ['Open Sans', 'sans-serif'].join(','),
     htmlFontSize: 10,
+    h1: {
+      fontFamily: ['Lato', 'sans-serif'].join(','),
+    },
+    h2: {
+      fontFamily: ['Lato', 'sans-serif'].join(','),
+    },
+    h3: {
+      fontFamily: ['Lato', 'sans-serif'].join(','),
+    },
+    h4: {
+      fontFamily: ['Lato', 'sans-serif'].join(','),
+    },
+    h5: {
+      fontFamily: ['Lato', 'sans-serif'].join(','),
+    },
+    h6: {
+      fontFamily: ['Lato', 'sans-serif'].join(','),
+    },
   },
 });
+
+baseTheme = responsiveFontSizes(baseTheme);
