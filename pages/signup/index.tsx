ee098643f929374e81cff2c5365dc9516f5b7804
atsteinberg <alexts404dev@gmail.com>
2020-12-19 13:35:39 +0100

    feat: add card to signup page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,11 +1,18 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
 import { Button } from '../../components/buttons';
 import { Layout } from '../../containers/layout';
+import text from '../../lib/text';
 
 const SignUpPage = (): React.ReactElement => {
   return (
     <Layout title="sign up">
-      <Button href="/talents/1/profile">Talents</Button>
-      <Button href="/employers/1/profile">Employers</Button>
+      <Card>
+        <CardContent>
+          <Typography variant="h2">
+            {text.german.pages.signup.header}
+          </Typography>
+        </CardContent>
+      </Card>
     </Layout>
   );
 };
