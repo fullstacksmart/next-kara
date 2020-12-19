f523d93e2d2241459d1a653540f36e1798b5ecae
atsteinberg <alexts404dev@gmail.com>
2020-12-19 13:35:16 +0100

    feat: add text object

---
+++ lib/text/index.ts
@@ -0,0 +1,14 @@
+export default {
+  german: {
+    pages: {
+      signup: {
+        header: 'Erstelle Dein Profil',
+      },
+    },
+    fullName: {
+      firstName: 'Vorname',
+      middleName: 'Zweiter Vorname',
+      lastName: 'Nachname',
+    },
+  },
+};
