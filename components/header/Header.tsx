5b1227a3f5f952c7e02888ba576ab8ca1cacbfdd
camifernweh <camiladmuller@gmail.com>
2020-12-20 01:22:24 +0100

    feat: add header

---
+++ components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Navbar from '../navbar/Navbar';
+
+const Header = (): React.ReactElement => {
+  return <Navbar />;
+};
+
+export default Header;
