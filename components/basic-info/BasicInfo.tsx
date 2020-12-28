80c706cf62d4a534a2c217b0eebaac0eb845b244
atsteinberg <alexts404dev@gmail.com>
2020-12-28 10:39:07 +0100

    feat: add BasicInfo component

---
+++ components/basic-info/BasicInfo.tsx
@@ -0,0 +1,49 @@
+import { Typography } from '@material-ui/core';
+import { SectionItem } from '../section-item/SectionItem';
+import { Section } from '../section/Section';
+import { CheckedTitle } from '../checked-title/CheckedTitle';
+import styles from './BasicInfo.module.css';
+
+interface BasicInfoProps {
+  profilePicUrl?: string;
+  fullName: string;
+  profession: string;
+  address: string;
+  description?: string;
+  done: boolean;
+  handleEdit: () => void;
+}
+
+export const BasicInfo = ({
+  profilePicUrl,
+  fullName,
+  profession,
+  address,
+  description,
+  done,
+  handleEdit,
+}: BasicInfoProps): React.ReactElement => {
+  return (
+    <div className={styles.BasicInfo}>
+      <Section>
+        <SectionItem handleEdit={handleEdit}>
+          <div className={styles.HeaderContainer}>
+            {profilePicUrl && (
+              <img
+                className={styles.ProfilePic}
+                src={profilePicUrl}
+                alt={fullName}
+              />
+            )}
+            <div className={styles.Details}>
+              <CheckedTitle title={fullName} done={done} />
+              <Typography variant="h4">{profession}</Typography>
+              <Typography variant="h4">{address}</Typography>
+            </div>
+          </div>
+          <div className={styles.Description}>{description}</div>
+        </SectionItem>
+      </Section>
+    </div>
+  );
+};
--- pages/talents/[id]/profile.tsx
+++ pages/talents/[id]/profile.tsx
@@ -3,8 +3,7 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import { Button } from '../../../components/buttons';
 import { Layout } from '../../../containers/layout';
 import { PageProps } from '../../../lib/types';
-import { Section } from '../../../components/section/Section';
-import { SectionItem } from '../../../components/section-item/SectionItem';
+import { BasicInfo } from '../../../components/basic-info/BasicInfo';
 
 export interface ProfilePageProps extends PageProps {
   id: string;
@@ -77,32 +76,27 @@ const ProfilePage = ({ id }: ProfilePageProps): React.ReactElement => {
   });
   if (loading) return <h1>Loading</h1>;
   if (error) return <h1>Error: {error.message}</h1>;
-  const { name, fullName, profilePic } = data?.getTalentById;
+  const {
+    name,
+    fullName,
+    profilePic,
+    profession,
+    address,
+    description,
+  } = data?.getTalentById;
   console.log(profilePic);
   return (
     <Layout title={['profile', `Talent ${id}`]}>
       <h1>Profile Page for Talent {name.firstName + ' ' + name.lastName}</h1>
-      <Section>
-        <SectionItem
-          handleEdit={() => {
-            console.log('edit');
-          }}
-        >
-          {fullName}
-          <div>Galina, oh Galina</div>
-
-          <div>la la la la la</div>
-          <div>Galina, oh Galina</div>
-
-          <div>la la la la la</div>
-          <div>Galina, oh Galina</div>
-
-          <div>la la la la la</div>
-          <div>Galina, oh Galina</div>
-
-          <div>la la la la la</div>
-        </SectionItem>
-      </Section>
+      <BasicInfo
+        fullName={fullName}
+        profilePicUrl={profilePic}
+        profession={profession}
+        address={address.city}
+        description={description}
+        done={true}
+        handleEdit={() => console.log('edit')}
+      />
       <Button href={`/talents/${id}/settings`}>To Settings</Button>
     </Layout>
   );
