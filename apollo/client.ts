bb6967587e81b0b282fb735f05384ff091a69acb
beckertobias <tobias.becker@scalable.capital>
2021-01-04 17:09:21 +0100

    feat: set token in header auth for each request

--- apollo/client.ts
+++ apollo/client.ts
@@ -4,21 +4,35 @@ import {
   InMemoryCache,
   NormalizedCacheObject,
 } from '@apollo/client';
-
-// const uri = '/api';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/api',
 });
 
-// link needed?
-// const link = ApolloLink.from([http]);
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const cache = new InMemoryCache();
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache
 });
 
 export default client;
+
+
+/*
+r@ownefewf.com
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImgzanlHdkdBWnJEa083cm5jcDRKRiIsInR5cGUiOiJUQUxFTlQiLCJpYXQiOjE2MDk3NzYyODZ9.8HLb4Oq3ovto405jWLepZwYNWJBjyG5w2XrvjH8nKso
+*/
