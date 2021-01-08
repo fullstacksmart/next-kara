bddbede7bc51c03f883bb932c8e3003008caa3cc
camifernweh <camiladmuller@gmail.com>
2021-01-08 22:50:19 +0100

    fix: add layout styles to remove gutters from HiDPI screens

--- containers/layout/Layout.tsx
+++ containers/layout/Layout.tsx
@@ -3,8 +3,8 @@ import Container from '@material-ui/core/Container';
 import { Button } from '../../components/buttons';
 import Head from 'next/head';
 import { getTitleString } from '../../lib/utils/strings';
-import text from '../../lib/text';
 import Footer from '../../components/footer/Footer';
+import useStyles from './LayoutStyles';
 
 export interface LayoutProps {
   home?: boolean;
@@ -17,13 +17,17 @@ const Layout = ({
   children,
   title,
 }: LayoutProps): React.ReactElement => {
+  const classes = useStyles();
+
   return (
-    <Container disableGutters>
+    <Container disableGutters className={classes.container}>
       <Head>
         <title>{getTitleString(title)}</title>
       </Head>
-      <header>{!home && <Button href="/">take me back home</Button>}</header>
-      <main>{children}</main>
+      <header className={classes.header}>
+        {!home && <Button href="/">take me back home</Button>}
+      </header>
+      <main className={classes.main}>{children}</main>
       {!home && <Footer />}
     </Container>
   );
