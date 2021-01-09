f092b88be350387f8902b9257e7703ca61a08c64
atsteinberg <alexts404dev@gmail.com>
2021-01-09 17:45:52 +0100

    feat: add computeNestedValue helper

--- lib/utils/arrays.ts
+++ lib/utils/arrays.ts
@@ -9,3 +9,21 @@ export const filterById = (
   if (itemsWithId.length > 0) return itemsWithId[0];
   throw new Error(`no array element with id ${id}`);
 };
+
+export const computeNestedValue = (
+  object: Record<string, unknown>,
+  nestings: string[],
+  newValue: unknown,
+  //eslint-disable-next-line @typescript-eslint/ban-types
+): object | undefined => {
+  if (!nestings.length)
+    throw new Error('invalid nesting. Supply a non-empty array of strings');
+  if (nestings.length === 1) return { ...object, [nestings[0]]: newValue };
+  return {
+    [nestings[0]]: computeNestedValue(
+      object[nestings[0]] as Record<string, unknown>,
+      nestings.slice(1),
+      newValue,
+    ),
+  };
+};
