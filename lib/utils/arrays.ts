68f4fd06d8dd0873c4bb6375b6ab1bac07d24535
atsteinberg <alexts404dev@gmail.com>
2021-01-09 19:14:05 +0100

    fix: include all info in  computeNestedValue helper

--- lib/utils/arrays.ts
+++ lib/utils/arrays.ts
@@ -15,11 +15,15 @@ export const computeNestedValue = (
   nestings: string[],
   newValue: unknown,
   //eslint-disable-next-line @typescript-eslint/ban-types
-): object | undefined => {
+): Record<string, unknown> => {
   if (!nestings.length)
     throw new Error('invalid nesting. Supply a non-empty array of strings');
-  if (nestings.length === 1) return { ...object, [nestings[0]]: newValue };
+  if (nestings.length === 1) {
+    return { ...object, [nestings[0]]: newValue };
+  }
+  if (!object[nestings[0]]) throw new Error('no object');
   return {
+    ...object,
     [nestings[0]]: computeNestedValue(
       object[nestings[0]] as Record<string, unknown>,
       nestings.slice(1),
