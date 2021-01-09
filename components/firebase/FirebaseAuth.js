a7d99a0b322d1f7aaa657dcf2af64521830a4d3b
beckertobias <tobias.becker@scalable.capital>
2021-01-09 18:10:54 +0100

    feat: firebase setup

---
+++ components/firebase/FirebaseAuth.js
@@ -0,0 +1,42 @@
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import initFirebase from '../../lib/auth/initFirebase';
+import { setUserCookie } from '../../lib/auth/userCookies';
+import { mapUserData } from '../../lib/auth/mapUserData';
+
+// Init the Firebase app.
+initFirebase();
+
+const firebaseAuthConfig = {
+  signInFlow: 'popup',
+  // Auth providers
+  // https://github.com/firebase/firebaseui-web#configure-oauth-providers
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false,
+    },
+  ],
+  signInSuccessUrl: '/',
+  credentialHelper: 'none',
+  callbacks: {
+    signInSuccessWithAuthResult: async ({ user }, redirectUrl) => {
+      const userData = await mapUserData(user);
+      setUserCookie(userData);
+    },
+  },
+};
+
+const FirebaseAuth = () => {
+  return (
+    <div>
+      <StyledFirebaseAuth
+        uiConfig={firebaseAuthConfig}
+        firebaseAuth={firebase.auth()}
+      />
+    </div>
+  );
+};
+
+export default FirebaseAuth;
---
+++ pages/authtest.tsx
@@ -0,0 +1,14 @@
+import FirebaseAuth from '../components/firebase/FirebaseAuth';
+
+const Auth = () => {
+  return (
+    <div>
+      <p>Sign in</p>
+      <div>
+        <FirebaseAuth />
+      </div>
+    </div>
+  )
+}
+
+export default Auth
