3b9696b2342b785b3f82c5d93e9523e766f0f889
beckertobias <tobias.becker@scalable.capital>
2021-01-11 13:30:04 +0100

    feat: basic server auth setup

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,12 +1,25 @@
 import { ApolloServer } from 'apollo-server-micro';
+import { verifyIdToken } from '../../lib/auth/firebaseAdmin';
+import { getUidFromToken } from '../../lib/auth/firebaseAdmin';
 import typeDefs from '../../apollo/typedefs';
 import resolvers from '../../apollo/resolvers';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context(ctx) {
-    return ctx;
+  async context({ req }) {
+    const auth = req.headers.authorization;
+    const token = auth.replace('Bearer ', '');
+    console.log('token received in server: ', token);
+    try {
+      //TO DO: put both functions in one or improve
+      await verifyIdToken(token);
+      const uid = await getUidFromToken(token);
+      console.log(uid);
+      return { token };
+    } catch (error) {
+      console.error('error in server', error);
+    }
   },
   playground: true,
   introspection: true,
