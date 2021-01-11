38020706fbaca1c49f8ca6f243e166da7a15d52c
beckertobias <tobias.becker@scalable.capital>
2021-01-11 18:53:40 +0100

    feat: start signin page

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,11 +1,62 @@
 import { Button } from '../../components/buttons';
+import { Card, CardContent, Typography, Container } from '@material-ui/core';
 import { Layout } from '../../containers/layout';
-import { withTranslation } from '../../i18n';
+import InputField from '../../components/input-field/InputField';
+import { useState } from 'react';
 import { PageProps } from '../../lib/types';
+import { withTranslation } from '../../i18n';
+import firebase from '../../components/firebase';
+import styles from './Signin.module.css';
 
 const SignInPage = ({ t }: PageProps): React.ReactElement => {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  });
+  const handleSubmit = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(formValues.email, formValues.password)
+      .then((response) => {
+        console.log(response.user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(error);
+      });
+  };
+
   return (
-    <Layout title="sign in" t={t}>
+    <Layout title="sign in">
+      <Card>
+        <CardContent>
+          <Container>
+            <Typography variant="h2">Sign In</Typography>
+            <form onSubmit={handleSubmit}>
+              <InputField
+                id="email"
+                type="email"
+                value={formValues.email}
+                label={t('email')}
+                setValue={setFormValues}
+                inputProps={{ className: styles.FormInput }}
+                required
+              />
+              <InputField
+                id="password"
+                value={formValues.password}
+                label={t('password')}
+                setValue={setFormValues}
+                type="password"
+                required
+              />
+              <Button type="submit">signin</Button>
+            </form>
+          </Container>
+        </CardContent>
+      </Card>
+
       <Button href="/talents/yasbiuycdbucoiuscboiucsiousc!@/profile">
         Talents
       </Button>
