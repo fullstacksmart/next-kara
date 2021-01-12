9554597b5467f4f645415d48426d291d8c83b88d
atsteinberg <alexts404dev@gmail.com>
2021-01-12 11:38:42 +0100

    feat: add delete button to item edit

--- components/experience-section/ExperienceEdit.tsx
+++ components/experience-section/ExperienceEdit.tsx
@@ -178,6 +178,11 @@ export const ExperienceEdit = ({
       formId="experienceForm"
       reset={() => setUpdatedExperience(experience)}
       mutate={(id ? update : add) as MutationFunction}
+      onDelete={
+        (() => {
+          console.log('delete', id);
+        }) as MutationFunction
+      }
       {...props}
     >
       <ProfessionRadio
