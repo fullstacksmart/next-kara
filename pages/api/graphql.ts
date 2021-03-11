0184823119b013964e602a4601ff8728f21bdedb
camifernweh <camila.dmuller@gmail.com>
2021-03-11 17:27:52 +0100

    feat: add db to context object

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server-micro';
 import { getUserFromToken } from '../../lib/auth/firebaseAdmin';
 import typeDefs from '../../apollo/typedefs';
 import resolvers from '../../apollo/resolvers';
+import { sequelize } from '../../sequelize/models';
 
 const server = new ApolloServer({
   typeDefs,
@@ -10,8 +11,9 @@ const server = new ApolloServer({
     const auth = req.headers.authorization;
     const token = auth.replace('Bearer ', '');
     try {
+      const db = await sequelize.authenticate();
       const user = getUserFromToken(token);
-      return { user };
+      return { user, db };
     } catch (error) {
       console.error('error in server', error); //eslint-disable-line no-console
     }
