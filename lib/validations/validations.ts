502d75c625b79f741842f348017e6786aa1e04c8
Eduardo Pinto <eduardoandrespo@gmail.com>
2021-03-13 10:27:49 +0100

    remove special char from pass validation

--- lib/validations/validations.ts
+++ lib/validations/validations.ts
@@ -2,12 +2,12 @@ import * as Yup from 'yup';
 
 // Min 8 characters. At least 1 capital, 1 number, 1 special char.
 
-const strongCombination = /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[#?!@$%^&*\-_]).{8,}$/;
+const strongCombination = /^(?=.*?[0-9])(?=.*?[A-Z]).{8,}$/;
 
 const passwordSchema = Yup.object().shape({
   password: Yup.string()
     .required('Mandatory field')
-    .matches(strongCombination, 'Please enter a valid password'),
+    .matches(strongCombination, 'Please enter a stronger password'),
 });
 
 export default passwordSchema;
