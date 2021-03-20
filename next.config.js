d47ea0bc7ef0c3efb6091a2f3040578c7c8f0545
beckertobias <tobias.becker@scalable.capital>
2021-03-20 14:20:56 +0100

    add dev setup

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 const { nextI18NextRewrites } = require('next-i18next/rewrites');
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 const localeSubpaths = {
   de: 'de',
   en: 'en',
 };
 
-module.exports = {
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
+module.exports = (phase) => {
+  let uid;
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    uid = 'fUl2uRrJV4hzg2r077loNCLDajL2';
+  }
+  return {
+    rewrites: async () => nextI18NextRewrites(localeSubpaths),
+    publicRuntimeConfig: {
+      localeSubpaths,
+    },
+    env: {
+      uid,
+    },
+  };
 };
