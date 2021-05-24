13b6fb93e1fb6801f1e05a58b85d5c55a9bbb8e9
TobiBecker <tobias.becker@scalable.capital>
2021-05-24 15:03:38 +0200

    separate layoutVar

--- apollo/cache.ts
+++ apollo/cache.ts
@@ -3,11 +3,13 @@ import { FirebaseError } from 'lib/types/auth';
 import { ReactiveVar } from '@apollo/react-hooks';
 import { InMemoryCache } from '@apollo/client';
 
-export const layoutVar: ReactiveVar<{
-  title: string;
-  heading: string;
-  error: FirebaseError | null;
-}> = makeVar({ title: '', heading: '', error: null as FirebaseError | null });
+export const layoutTitleVar: ReactiveVar<string> = makeVar('');
+
+export const layoutHeadingVar: ReactiveVar<string> = makeVar('');
+
+export const layoutErrorVar: ReactiveVar<FirebaseError | null> = makeVar(
+  null as FirebaseError | null,
+);
 
 const cache = new InMemoryCache();
 
