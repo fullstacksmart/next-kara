79eff09d58cd8494e91d776b9ede868c1847f8ab
TobiBecker <tobias.becker@scalable.capital>
2021-07-30 10:25:19 +0200

    fix firebase storage bucket url

--- lib/auth/initFirebase.ts
+++ lib/auth/initFirebase.ts
@@ -7,7 +7,7 @@ const config = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: `gs://${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}.appspot.com`,
+  storageBucket: `gs://${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}`,
 };
 
 export default function initFirebase(): void {
